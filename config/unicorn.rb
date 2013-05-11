0bd93b71db07e114f25d74924a9341b742bcc927
Wonsup Lee <daul0212@gmail.com>
2013-05-11 21:45:35 +0900

    add deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+root = "/home/daul/repang/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+ 
+# change the YOUR_APP_NAME to your application name
+listen "/tmp/unicorn.repang.sock"
+worker_processes 2
+timeout 30
+ 
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = '/mnt/Hipstaweb-<%= RUBBER_ENV %>/current/Gemfile'
+end
