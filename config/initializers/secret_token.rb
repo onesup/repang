3a96b581c3f81caa279f84fdb3db49e956bd6311
Wonsup Lee <daul0212@gmail.com>
2013-05-11 15:06:09 +0900

    created repang project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repang::Application.config.secret_token = '805b9ce109e46d4a1bb3f98dc1360e13451c682e6d3c03114fc2da0f85e0dd118b0e6cab54b11e9025a279fd398077711aac5aa82d429cf37d8a7c80ff405ed3'
