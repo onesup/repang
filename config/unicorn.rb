5bc3d2db9bff2563ed91f01acd1f4730c08984ae
Wonsup Lee <daul0212@gmail.com>
2013-05-13 23:57:18 +0900

    bundle error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/daul/repang/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn.err.log"
+stdout_path "#{root}/log/unicorn.out.log"
  
 # change the YOUR_APP_NAME to your application name
 listen "/tmp/unicorn.repang.sock"
@@ -10,5 +10,5 @@ worker_processes 2
 timeout 30
  
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = '/mnt/Hipstaweb-<%= RUBBER_ENV %>/current/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = "<%= current_path %>/Gemfile"
 end
