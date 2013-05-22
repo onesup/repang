b9ef18d88aa4e1245ec55d64c5f005f527b1c553
Wonsup Lee <daul0212@gmail.com>
2013-05-22 20:01:13 +0900

    모달 레이

--- app/assets/javascripts/livere.js
+++ app/assets/javascripts/livere.js
@@ -1,18 +1,4 @@
-/*
- * SimpleModal Basic Modal Dialog
- * http://simplemodal.com
- *
- * Copyright (c) 2013 Eric Martin - http://ericmmartin.com
- *
- * Licensed under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
- */
-
 jQuery(function ($) {
-	// Load dialog on page load
-	//$('#basic-modal-content').modal();
-
-	// Load dialog on click
 	$('.register_button').click(function (e) {
 		$('#register_form').modal();
 
