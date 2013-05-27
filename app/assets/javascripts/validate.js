da366ce6a9c7a1d723a84d0e9db6b690fc92c66a
Wonsup Lee <daul0212@gmail.com>
2013-05-27 17:46:21 +0900

    validation 등록 form

---
+++ app/assets/javascripts/validate.js
@@ -0,0 +1,27 @@
+$().ready(function() {
+	$("#new_user").validate({
+
+		rules: {
+			"user[name]": "required",
+			"user[email]": {
+				required: true,
+				email: true
+			},
+			"user[phone]": "required",
+			agree_private_information: "required",
+			agree_private_number: "required"			
+		},
+
+		messages: {
+			"user[name]": "이름을 남겨주세요.",
+			"user[email]": {
+				required: "이메일 주소를 남겨주세요.",
+				email: "주소를 확인해주세요."
+			},
+			"user[phone]": "전화번호를 남겨주세요.",
+			agree_private_information: "동의가 필요합니다.",
+			agree_private_number: "동의가 필요합니다."
+
+		}
+	});
+});
