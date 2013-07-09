99db1fe1abd0fce88eda88875b04b9b19fcbc279
Wonsup Lee <daul0212@gmail.com>
2013-07-10 03:49:12 +0900

    modal layer 여는 방식 변경

--- app/assets/javascripts/livere.js
+++ app/assets/javascripts/livere.js
@@ -1,6 +1,11 @@
 jQuery(function ($) {
+	livere();
+});
+
+function livere() {
 	$('.register_button').click(function (e) {
 		$('#register_form').modal();
+	  // $("#register_form").slideDown();		
 		return false;
-	});
-});
+	});	
+}
