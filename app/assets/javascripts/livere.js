f09979505039558a71c6a92939ff5ac40e0edfbe
Wonsup Lee <daul0212@gmail.com>
2013-07-10 04:03:25 +0900

    modal layer 관련

--- app/assets/javascripts/livere.js
+++ app/assets/javascripts/livere.js
@@ -5,7 +5,8 @@ jQuery(function ($) {
 function livere() {
 	$('.register_button').click(function (e) {
 		$('#register_form').modal();
-	  // $("#register_form").slideDown();		
+	  // $("#register_form").slideDown();
+		location.href="#register_form"
 		return false;
 	});	
 }
