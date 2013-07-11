debe8053849f1c52b757ef8d072035c1063e39b4
Wonsup Lee <daul0212@gmail.com>
2013-07-11 18:58:11 +0900

    fix livere js

--- app/assets/javascripts/livere.js
+++ app/assets/javascripts/livere.js
@@ -1,14 +1,4 @@
-jQuery(function ($) {
-	livere();
-});
-
 function livere() {
-	// $('.register_button').click(function (e) {
 		$('#register_form').modal();
-	  // $("#register_form").slideDown();
 		return false;
-		// $('.close_button').click(function (e) {
-		// 	$("#register_form").hide();
-		// });
-	// });	
 }
